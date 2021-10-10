cb861fdfe901b3612758df3ce4b67f2be53dabae
Yusufbek Orzibekov <75078482+yusuforzibekov@users.noreply.github.com>
2021-10-10 11:48:00 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+a = int(input('a = '))
+b = int(input('b = '))
+amal = input('add/sub/mul/div:')
+if amal == 'add':
+    c = a + b
+elif amal == 'sub':
+    c = a - b
+elif amal == 'mul':
+    c = a * b
+elif amal == 'div':
+    c = a / b
+else:
+    c = 'Error'
+print('Result =', c)
